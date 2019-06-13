69632ed76d3a64171680ac251ed5088ddc1f7191
blubberpie <lozberipie@gmail.com>
2019-06-13 17:10:25 +0700

    Create threadpool

--- threadpool.c
+++ threadpool.c
@@ -12,23 +12,23 @@
 #include "threadpool.h"
 
 // A queue of functions to be executed
-typedef struct _function_st {
+typedef struct function_st {
     void (*task) (void*); // The function to be executed
     void *arg;
-    struct _function_st* next;
-} _function_t;
+    struct function_st* next;
+} function_t;
 
 // _threadpool is the internal threadpool structure that is
 // cast to type "threadpool" before it given out to callers
 typedef struct _threadpool_st {
-    int max_threads;
-    int min_threads;
     int thread_count;
     pthread_t *threads;
     pthread_mutex_t lock;
+    pthread_cond_t busy;
+    pthread_cond_t empty;
     int function_count;
-    _function_t task_head;
-    _function_t task_tail;
+    function_t* task_head;
+    function_t* task_tail;
 } _threadpool;
 
 
@@ -56,7 +56,23 @@ threadpool create_threadpool(int num_threads_in_pool) {
     return NULL;
   }
 
-  // add your code here to initialize the newly created threadpool
+  // allocate thread pool with num_threads_in_pool
+  pool->threads = (pthread_t*) malloc(sizeof(pthread_t) * num_threads_in_pool);
+
+  // todo: if(!pool->threads) out of memory blah blah
+
+  pool->thread_count = num_threads_in_pool;
+  pool->task_head = NULL;
+  pool->task_tail = NULL;
+  pthread_mutex_init(&pool->lock, NULL); // todo: (void) ???
+  pthread_cond_init(&pool->busy, NULL);
+  pthread_cond_init(&pool->empty, NULL);
+
+  int i;
+  for(i = 0; i < num_threads_in_pool; i++){
+      int error = pthread_create(&(pool->threads[i]), NULL, worker_thread, pool);
+      if (error) return NULL; // Thread couldn't be created
+  }
 
   return (threadpool) pool;
 }
@@ -66,7 +82,7 @@ void dispatch(threadpool from_me, dispatch_fn dispatch_to_here,
 	      void *arg) {
   _threadpool *pool = (_threadpool *) from_me;
 
-  // add your code here to dispatch a thread
+
 }
 
 void destroy_threadpool(threadpool destroyme) {
