306b208c12417045deedf793ab3859fb88bb3776
blubberpie <lozberipie@gmail.com>
2019-06-13 17:45:32 +0700

    Dispatch

--- threadpool.c
+++ threadpool.c
@@ -12,11 +12,11 @@
 #include "threadpool.h"
 
 // A queue of functions to be executed
-typedef struct function_st {
-    void (*task) (void*); // The function to be executed
+typedef struct task_st {
+    void (*function) (void*); // The function to be executed
     void *arg;
-    struct function_st* next;
-} function_t;
+    struct task_st* next;
+} task_t;
 
 // _threadpool is the internal threadpool structure that is
 // cast to type "threadpool" before it given out to callers
@@ -25,10 +25,11 @@ typedef struct _threadpool_st {
     pthread_t *threads;
     pthread_mutex_t lock;
     pthread_cond_t busy;
+    pthread_cond_t occupied;
     pthread_cond_t empty;
-    int function_count;
-    function_t* task_head;
-    function_t* task_tail;
+    int task_count;
+    task_t* task_head;
+    task_t* task_tail;
 } _threadpool;
 
 
@@ -66,6 +67,7 @@ threadpool create_threadpool(int num_threads_in_pool) {
   pool->task_tail = NULL;
   pthread_mutex_init(&pool->lock, NULL); // todo: (void) ???
   pthread_cond_init(&pool->busy, NULL);
+  pthread_cond_init(&pool->occupied, NULL);
   pthread_cond_init(&pool->empty, NULL);
 
   int i;
@@ -82,7 +84,27 @@ void dispatch(threadpool from_me, dispatch_fn dispatch_to_here,
 	      void *arg) {
   _threadpool *pool = (_threadpool *) from_me;
 
+    task_t *current_task = (task_t*) malloc(sizeof(task_t));
 
+    if (current_task == NULL) return; // Can't create task struct
+
+    current_task->function = dispatch_to_here;
+    current_task->arg = arg;
+    current_task->next = NULL;
+
+    pthread_mutex_lock(&(pool->lock));
+
+    if (pool->task_count == 0){
+        pool->task_head = current_task;
+        pool->task_tail = current_task;
+        pthread_cond_signal(&pool->occupied);
+    }else{ // todo: check
+        pool->task_tail->next = current_task;
+        pool->task_tail = current_task;
+    }
+    pool->task_count++;
+
+    pthread_mutex_unlock(&(pool->lock));
 }
 
 void destroy_threadpool(threadpool destroyme) {
